3444f0033827548b20922a1d7ee40ccdfb020486
rahulrajdahal <rahulrajdahal@gmail.com>
2024-08-02 18:27:27 +0545

    fix: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      manifest: {
+        name: "Recyclr",
+        short_name: "recyclr",
+        description: "Let’s save the world TOGETHER.",
+        start_url: "/",
+        theme_color: "#ffffff",
+      },
+      pwaAssets: {
+        config: true,
+        overrideManifestIcons: true,
+      },
+    }),
+  ],
+});
