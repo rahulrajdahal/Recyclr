b4619b4238a04b25829be9334dab8eaa51e491bb
Rahul Raj Dahal <rahulrajdahal@gmail.com>
2025-11-22 00:18:44 +0000

    build: footer storybook

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { Facebook, Twitter, Youtube } from "../assets/icons";
+import { Facebook, Twitter, Youtube } from "../../assets/icons";
 
 const socials = [
   { id: 1, title: "Facebook", icon: Facebook },
@@ -8,7 +8,7 @@ const socials = [
 
 export default function Footer() {
   return (
-    <footer className="bg-[#121212] px-[4.65%] md:px-[12.5%] w-full flex py-10 items-center justify-between flex-wrap">
+    <footer className="bg-[#121212] px-[4.65%] xl:px-[12.5%] w-full flex py-10 items-center justify-between flex-wrap">
       <p className="text-white text-lg leading-6 font-semibold">
         Copyright. 2020. All Rights Reserved.
       </p>
